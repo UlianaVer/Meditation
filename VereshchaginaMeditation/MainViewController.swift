b5d23fd8437f898bcfb929d0642f831c509c3790
Верещагина Ульяна Николаевна <vereshchagina.u.n@mac402-04.local>
2022-11-24 10:03:51 +0300

    Final ver.

---
+++ VereshchaginaMeditation/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  VereshchaginaMeditation
+//
+//  Created by Верещагина Ульяна Николаевна on 24/11/2022.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+}
